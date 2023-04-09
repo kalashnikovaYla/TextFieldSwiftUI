bc8ad51b97785e313f81078c532c6a87af19c431
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 17:24:06 +0500

    Initial Commit

---
+++ TextFieldSwiftUI/TextFieldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldSwiftUIApp.swift
+//  TextFieldSwiftUI
+//
+//  Created by sss on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
