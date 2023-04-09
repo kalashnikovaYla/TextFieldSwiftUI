489e39017707c205280fd2163ba448c0cdd0275f
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 18:19:24 +0500

    Initial commit

--- TextFieldSwiftUI/ContentView.swift
+++ TextFieldSwiftUI/ContentView.swift
@@ -8,15 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
+    @FocusState var nameIsFocused: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 30) {
+            
+            TextField("Enter login", text: $text).redTextField().focused($nameIsFocused)
+            TextField("Enter password", text: $text).modifier(TextFieldModifier())
+
+            Button {
+                nameIsFocused = false
+                //hideKeyboard()
+            } label: {
+                Text("Log in")
+            }
+
         }
         .padding()
     }
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+extension View {
+    func redTextField() -> some View {
+        modifier(TextFieldModifier())
+    }
+}
+
+
+struct TextFieldModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+            content
+            .textFieldStyle(.roundedBorder)
+            .tint(.accentColor)
+            .font(.system(size: 24))
+            .keyboardType(.decimalPad)
+            .padding(.horizontal, 12)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(.blue)
+            }
+            .foregroundColor(Color.blue)
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +64,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+  
